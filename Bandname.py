f5235424f829ee4227f29cdfd2c19a314f641a72
Manas Ranjan Sahu <manassensei@gmail.com>
2024-07-18 07:00:07 +0530

    Create Bandname.py

---
+++ Bandname.py
@@ -0,0 +1,4 @@
+print('Welcome to the band name generator')
+city = input("Which city did you grow up in? ")
+animal = input("the animal you like? ")
+print("Your band name could be " + city +" "+ animal)
